5a0838d22949765b1d7b88718519e47c71686598
Tohru Yaginuma <toru45s@gmail.com>
2025-09-17 13:30:11 -0700

    completed

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
@@ -14,12 +15,35 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 	e:= echo.New()
 	e.Debug = true
 	e.HideBanner = true
-
 	e.Logger.SetLevel(log.DEBUG)
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowHeaders: []string{echo.HeaderOrigin, 
+			echo.HeaderContentType, 
+			echo.HeaderAccept, 
+			echo.HeaderAccessControlAllowHeaders, 
+			echo.HeaderXCSRFToken,
+		},
+		AllowMethods: []string{"GET", "PUT", "POST", "DELETE"},
+		AllowCredentials: true,
+	}))
+
+	e.Use(middleware.CSRFWithConfig(
+		middleware.CSRFConfig{
+			CookiePath: "/",
+			CookieDomain: os.Getenv("API_DOMAIN"),
+			CookieHTTPOnly: true,
+			CookieSameSite: http.SameSiteDefaultMode,
+			// CookieSameSite: http.SameSiteNoneMode,
+			// CookieMaxAge: 60,
+		},
+	))
 
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	e.GET("/csrf", uc.CsrfToken)
 
 	t := e.Group("/tasks")
 
